15ff4c07facdd2f78edc88108b6374fc1f3fdc51
layan18-meet <mlayan147@gmail.com>
2016-12-15 18:31:23 +0200

    Made a class to describe a student

---
+++ student.py
@@ -0,0 +1,11 @@
+class Student :
+    def __init__(self, hometown, age, height, fav_icecream):
+        self.hometown= hometown
+        self.age= age
+        self.height= height
+        self.fav_icecream= fav_icecream
+    def print_summary(self)
+        print("Hometown: "+self.hometown+" age: "+self.age+" height: "+self.height+" age: "+self.age+" favorite ice cream flavor: "+self.fav_icecream)
+
+    def get_giraffe_gap(self)
+        return(500-self.height)
